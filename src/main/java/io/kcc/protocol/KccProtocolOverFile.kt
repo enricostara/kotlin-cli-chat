9f52f762f4b505c2f8c234a1705e0129b51967fd
Enrico Stara <enrico.stara@gmail.com>
2021-03-16 07:01:03 +0100

    Implement KccProtocolOverFile::readTopics method and test

--- src/main/java/io/kcc/protocol/KccProtocolOverFile.kt
+++ src/main/java/io/kcc/protocol/KccProtocolOverFile.kt
@@ -4,6 +4,7 @@ import io.kcc.model.Host
 import io.kcc.model.Message
 import io.kcc.model.Topic
 import io.kcc.model.User
+import java.io.File
 
 object KccProtocolOverFile : KccProtocol {
 
@@ -17,9 +18,12 @@ object KccProtocolOverFile : KccProtocol {
         else -> false
     }
 
-    override fun readTopics(): Set<Topic> {
-        TODO("Not yet implemented")
-    }
+    override fun readTopics(): Set<Topic> =
+        File(host.url.path).walk()
+            .filter { it.extension == "kcc" }
+            .map { Topic(it.name.substringAfter('.').substringBeforeLast('.')) }
+            .toSet()
+
 
     override fun createTopic(topic: Topic, user: User) {
         TODO("Not yet implemented")
--- src/test/kotlin/io/kcc/protocol/KccProtocolOverFileTest.kt
+++ src/test/kotlin/io/kcc/protocol/KccProtocolOverFileTest.kt
@@ -1,9 +1,17 @@
 package io.kcc.protocol
 
 import io.kcc.model.Host
+import io.kcc.model.Topic
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import java.io.File
 
 internal class KccProtocolOverFileTest {
+
+    @AfterEach
+    fun clean() {
+        File("./").walk().filter { it.extension == "kcc" }.forEach { it.delete() }
+    }
 
     @Test
     fun accept() {
@@ -13,6 +21,11 @@ internal class KccProtocolOverFileTest {
 
     @Test
     fun readTopics() {
+        KccProtocolOverFile.accept(Host("file:./"))
+        File(".kotlin.kcc").createNewFile()
+        File(".java.kcc").createNewFile()
+        val topics = KccProtocolOverFile.readTopics()
+        assert(setOf(Topic("java"), Topic("kotlin")).containsAll(topics))
     }
 
     @Test
